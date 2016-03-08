df3ec2aff5a73c85bc1ae495227ba3f044744037
Steve Pentler <stevepentler@gmail.com>
2016-03-08 09:20:40 -0700

    ItemsController index test passing

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
+  respond_to :json
   def index
+    respond_with Item.all
   end
 end
--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,23 +1,25 @@
 require 'test_helper'
 
-class Api::V1::ItemsControllerTest < ActiveSupport::TestCase
-  item1 = Item.create(name: "banana",
-                      description: "yellow",
-                      created_at: "5AM",
-                      updated_at: "5PM"
-                      )
-  item2 = Item.create(name: "boomerang",
-                      description: "flies"
-                      )
+class Api::V1::ItemsControllerTest < ActionController::TestCase
+  test '#index' do 
+    item1 = Item.create(name: "banana",
+                        description: "yellow",
+                        created_at: "5AM",
+                        updated_at: "5PM"
+                        )
+    item2 = Item.create(name: "boomerang",
+                        description: "flies"
+                        )
 
-  get :index, format: :json
+    get :index, format: :json
 
-  items = JSON.parse(response.body)
+    items = JSON.parse(response.body)
 
-  assert_repsonse :success
+    assert_response :success
 
-  assert_equal item1.name, items.first["name"]
-  assert_equal item1.description, items.first["description"]
-  assert_equal item2.name, items.last["name"]
-  assert_equal item2.description, items.last["description"]
+    assert_equal item1.name, items.first["name"]
+    assert_equal item1.description, items.first["description"]
+    assert_equal item2.name, items.last["name"]
+    assert_equal item2.description, items.last["description"]
+  end
 end
