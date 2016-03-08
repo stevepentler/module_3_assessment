6090208e2bc558c428a203e2a6f4df9560eb117f
Steve Pentler <stevepentler@gmail.com>
2016-03-08 09:30:36 -0700

    Renders index without timestamps

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   respond_to :json
   def index
-    respond_with Item.all
+    respond_with Item.all, except: [:updated_at, :created_at]
   end
 end
