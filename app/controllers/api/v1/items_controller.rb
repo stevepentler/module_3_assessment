de132caca4044061dab108d451f77a1a2eb0b3a3
Steve Pentler <stevepentler@gmail.com>
2016-03-08 09:49:20 -0700

    creates select_attributes methods in items_controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::ItemsController < ApplicationController
   respond_to :json
   def index
-    respond_with Item.all, except: [:updated_at, :created_at]
+    respond_with Item.all, select_attributes
   end
 
   def show
     item = Item.find(params[:id])
-    respond_with item
+    respond_with item, select_attributes
+  end
+
+  private
+
+  def select_attributes
+    {except: [:updated_at, :created_at]}
   end
 end
